0ccd9952ff5bb5f3370d6a48e78c493d4f600a80
Blackmoor <anegka@hotmail.com>
2015-10-03 00:25:32 +0300

    Добавлено первое демо звука по анимации
    Лифт обладает своим звуком, воспроизводится по нажатию на клавишу Е. На
    экран при попадании в триггер вводится подсказка по клавише, которую
    следует нажать.

--- Assets/scripts/AnimateOnButton.cs
+++ Assets/scripts/AnimateOnButton.cs
@@ -3,24 +3,54 @@ using System.Collections;
 
 public class AnimateOnButton : MonoBehaviour {
 
-	public Animator FirstAnimation; // анимация, которая проигрывается первой
-	public Animator SecondAnimation; // анимация, которая запускается второй
+	//public Animator FirstAnimation; // анимация, которая проигрывается первой
+	//public GameObject SecObject;
+	public GameObject SecondAnimation; // анимация, которая запускается второй
+	public GameObject HelpTooltip;
 	// Use this for initialization
+
+
 	void Start () {
-		SecondAnimation.animation.wrapMode = WrapMode.Loop;	
+		//FirstAnimation = GetComponent<Animator>();
+		//SecondAnimation = GetComponent<Animator>();
+		//FirstAnimation.animation.Stop ();
+		SecondAnimation.animation.Stop ();
+		//SecondAnimation.animation.wrapMode = WrapMode.Loop;	
 	}
 
-	
-	// Update is called once per frame
-	void OnTrigger(Collider collision){
-		if (collision.collider.tag == "MainCamera") {
+	/*void OnUpdate()
+	{
+		if (!SecondAnimation.animation.isPlaying & SecondAnimation.audio.isPlaying) {
+			SecondAnimation.audio.Stop();
+		} else {
+			if (SecondAnimation.animation.isPlaying & !SecondAnimation.audio.isPlaying){
+				//Debug.Log(SecondAnimation.audio.isActiveAndEnabled);
+				SecondAnimation.audio.Play();
+				//Debug.Log(SecondAnimation.audio.isPlaying);
+			}
+		}
+
+	}
+*/
+	void OnTriggerEnter(Collider collision){
+		HelpTooltip.SetActive (true);
+	}
+	void OnTriggerExit(Collider collision){
+		HelpTooltip.SetActive (false);
+	}
+	void OnTriggerStay(Collider collision){
+		if (collision.CompareTag ("MainCamera")) {
 			if (!SecondAnimation.animation.isPlaying & Input.GetKeyUp (KeyCode.E)) {
-				FirstAnimation.animation.Play ();
-				SecondAnimation.animation.Play ();
+				//FirstAnimation.animation.Play ();
+				//Debug.Log("WHAAAAAT");
+				SecondAnimation.animation.Play();
+				//SecondAnimation.audio.Play();
+
 			} else {
 				if (SecondAnimation.animation.isPlaying & Input.GetKeyUp (KeyCode.E)) {
-					FirstAnimation.animation.Stop ();
-					SecondAnimation.animation.Play ();
+					//FirstAnimation.animation.Stop ();
+					SecondAnimation.animation.Stop();
+					//SecondAnimation.audio.Stop();
 				}
 			}
 		}
